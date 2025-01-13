b201e7e3db5507139b6eb65db031edc6c14fe10b
Maria Öhrman <maria.ohrman@gu.se>
2025-01-13 14:12:00 +0100

    load models at startup and only allow access to one thread at a time

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
 from fastapi import FastAPI
 from gensim.models.fasttext import FastText
+from queue import Queue
 
 app = FastAPI()
 
 corpus_lookup = {
-  "gp": "gp-2013-2022",
-  "dn": "dn-2010-2022",
-  "aftonbladet": "afb-2010-2022",
+    "gp": "gp-2013-2022",
+    "dn": "dn-2010-2022",
+    "aftonbladet": "afb-2010-2022",
+}
+types = ["lemma", "token"]
+
+
+def create_model(corpus, type) -> Queue:
+    model_name = f"kubord-fasttext-{corpus_lookup[corpus]}-{type}"
+    print(f"loading {model_name}")
+    model = FastText.load(f"models/{model_name}/{model_name}.bin")
+    q = Queue(maxsize=1)
+    q.put(model)
+    return q
+
+
+model_pool = {
+    corpus: {type: create_model(corpus, type) for type in types}
+    for corpus in corpus_lookup.keys()
 }
 
-models = {}
 
-def get_model(corpus, typ):
-    model_name = f"kubord-fasttext-{corpus_lookup[corpus]}-{typ}"
-    if model_name in models:
-        return models[model_name]
-    model = FastText.load(f"models/{model_name}/{model_name}.bin")
-    models[model_name] = model
-    return model
-    
+def get_model(corpus, type):
+    pool = model_pool[corpus][type]
+    return pool.get(block=True, timeout=5)
+
+
+def return_model(corpus, type, model):
+    model_pool[corpus][type].put(model)
 
 
 @app.get("/most_similar/{word}")
 def read_root(word, corpus=None, type=None):
     model = get_model(corpus, type)
-    return model.wv.most_similar(word, topn=10)
+    res = model.wv.most_similar(word, topn=10)
+    return_model(corpus, type, model)
+    return res
