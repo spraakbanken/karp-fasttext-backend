05bae4d35854db53c5fdb9c2045e2b8cb7bdcc87
Maria Öhrman <maria.ohrman@gu.se>
2025-01-14 08:48:32 +0100

    rename parameter

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
+import logging
+from queue import Queue
+
 from fastapi import FastAPI
 from gensim.models.fasttext import FastText
-from queue import Queue
 
 app = FastAPI()
 
-corpus_lookup = {
+newspaper_lookup = {
     "gp": "gp-2013-2022",
     "dn": "dn-2010-2022",
     "aftonbladet": "afb-2010-2022",
 }
+
+logger = logging.Logger("main")
+
 types = ["lemma", "token"]
 
 
-def create_model(corpus, type) -> Queue:
-    model_name = f"kubord-fasttext-{corpus_lookup[corpus]}-{type}"
-    print(f"loading {model_name}")
+def create_model(newspaper, type) -> Queue:
+    model_name = f"kubord-fasttext-{newspaper_lookup[newspaper]}-{type}"
+    logger.info(f"loading {model_name}")
     model = FastText.load(f"models/{model_name}/{model_name}.bin")
     q = Queue(maxsize=1)
     q.put(model)
@@ -22,23 +27,23 @@ def create_model(corpus, type) -> Queue:
 
 
 model_pool = {
-    corpus: {type: create_model(corpus, type) for type in types}
-    for corpus in corpus_lookup.keys()
+    newspaper: {type: create_model(newspaper, type) for type in types}
+    for newspaper in newspaper_lookup.keys()
 }
 
 
-def get_model(corpus, type):
-    pool = model_pool[corpus][type]
+def get_model(newspaper, type):
+    pool = model_pool[newspaper][type]
     return pool.get(block=True, timeout=5)
 
 
-def return_model(corpus, type, model):
-    model_pool[corpus][type].put(model)
+def return_model(newspaper, type, model):
+    model_pool[newspaper][type].put(model)
 
 
 @app.get("/most_similar/{word}")
-def read_root(word, corpus=None, type=None):
-    model = get_model(corpus, type)
+def read_root(word, newspaper=None, type=None):
+    model = get_model(newspaper, type)
     res = model.wv.most_similar(word, topn=10)
-    return_model(corpus, type, model)
+    return_model(newspaper, type, model)
     return res
