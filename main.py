35992875f8439181c2fc13bc0e098854cd40ccb5
Maria Öhrman <maria.ohrman@gu.se>
2025-01-14 11:32:38 +0100

    links to korp

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ from gensim.models.fasttext import FastText
 
 app = FastAPI()
 
+newspaper_links = {
+    "gp": "https://spraakbanken.gu.se/korp/?mode=kubord#?cqp=%5B%5D&corpus=kubord2-gp-2013,kubord2-gp-2014,kubord2-gp-2015,kubord2-gp-2016,kubord2-gp-2017,kubord2-gp-2019,kubord2-gp-2021,kubord2-gp-2018,kubord2-gp-2020,kubord2-gp-2022&result_tab=2&show_stats&search=word|",
+    "dn": "https://spraakbanken.gu.se/korp/?mode=kubord#?cqp=%5B%5D&corpus=kubord2-dn-2010,kubord2-dn-2011,kubord2-dn-2012,kubord2-dn-2013,kubord2-dn-2014,kubord2-dn-2015,kubord2-dn-2016,kubord2-dn-2017,kubord2-dn-2018,kubord2-dn-2019,kubord2-dn-2020,kubord2-dn-2021,kubord2-dn-2022&result_tab=2&show_stats&search=word|",
+    "aftonbladet": "https://spraakbanken.gu.se/korp/?mode=kubord#?cqp=%5B%5D&corpus=kubord2-afb-2010,kubord2-afb-2011,kubord2-afb-2012,kubord2-afb-2013,kubord2-afb-2014,kubord2-afb-2016,kubord2-afb-2017,kubord2-afb-2018,kubord2-afb-2019,kubord2-afb-2020,kubord2-afb-2021,kubord2-afb-2015,kubord2-afb-2022&result_tab=2&show_stats&search=word|",
+}
+
 newspaper_lookup = {
     "gp": "gp-2013-2022",
     "dn": "dn-2010-2022",
@@ -74,7 +80,13 @@ def read_root(searches, newspaper=None, type=None, size=10, format="html"):
         tables = []
         for res in results:
             table_rows = [
-                f"<tr><td>{row[0]}</td><td>{row[1]}</td></tr>" for row in res[1]
+                f"""<tr>
+                        <td>
+                            <a href="{newspaper_links[newspaper] + row[0]}" target="_blank">{row[0]}</a>
+                        </td>
+                        <td>{row[1]}</td>
+                    </tr>"""
+                for row in res[1]
             ]
             title = ", ".join(res[0])
             table = "<h2>" + title + "</h2><table>" + "".join(table_rows) + "</table>"
